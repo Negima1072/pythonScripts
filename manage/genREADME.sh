6afe8cba9c4ce2d20ad42d63a0619802e7c1eabd
Negima1072 <tkser@icloud.com>
2021-02-25 19:03:14 +0900

    f

--- manage/genREADME.sh
+++ manage/genREADME.sh
@@ -18,6 +18,6 @@ cat <<EOF > $OUTFILE
 $(cat README.head.md | tr -d \\r)
 $(extract_summary ${INFILES[@]})
 $(cat README.foot.md | tr -d \\r)
-$(echo "\n" && date +"Updt:%Y/%m/%d %I:%M:%S")
+$(echo \e "\n\n" && date +"Updt: %Y/%m/%d %I:%M:%S by genREADME")
 EOF
 git add $OUTFILE
