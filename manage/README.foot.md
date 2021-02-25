90350eee989cb1b0f1b73539682a379761c2debe
Negima1072 <tkser@icloud.com>
2021-02-25 19:35:37 +0900

    f

--- manage/README.foot.md
+++ manage/README.foot.md
@@ -1,5 +1,4 @@
 
 ## Author
-Copyright (c) 2021 Negima1072 under the MIT License.
+Copyright (c) 2021 [Negima1072](https://twitter.com/Negima1072) under the MIT License.
 
-Negima1072 ([@Negima1072](https://twitter.com/Negima1072))
--- manage/genREADME.sh
+++ manage/genREADME.sh
@@ -19,6 +19,6 @@ $(cat README.head.md | tr -d \\r)
 $(extract_summary ${INFILES[@]})
 $(cat README.foot.md | tr -d \\r)
 
-$(TZ=-9 date +"Update: %Y/%m/%d %I:%M:%S by [genREADME](manage/genREADME.sh)")
+$(timedatectl set-timezone Asia/Tokyo && date +"Update: %Y/%m/%d %I:%M:%S by [genREADME](manage/genREADME.sh)")
 EOF
 git add $OUTFILE
